f60d0f93b424d337c122bfd03e59a68a1a5715d8
Navneeta Sinha <72189093+navneeta2020@users.noreply.github.com>
2022-09-12 18:00:05 +0530

    Create ide_python.py

---
+++ ide_python.py
@@ -0,0 +1,107 @@
+
+III. IDE PyCharm:
+===================
+1. Different IDEs in market 
+   IDE (Integrated Development Environment) is a place where programmers can create their own applications by writing the code.
+   Normally the IDEs come with debuggers and compilers, which makes the work of programmers much easier. It is a single software 
+   in which all parts of development are done. In earlier days the IDEs started with terminals and consoles, but later they have 
+   changed a lot due to user needs. Nowadays the IDEs are much more developed, they use machine learning to autocomplete our code 
+   and also provide suggestions while we are struck or face any issues.
+
+1. IntelliJ IDEA
+   It is written in Java and is one of the most user-friendly IDEs available for coding. It mainly aims at productivity and user 
+   experience. It gives relevant suggestions while you are coding. It has integrated version control systems and it supports many 
+   languages. It has smart code completion which suggests the names of classes and methods while we are coding. It does not require
+   the use of any external plugins. Intellij IDEA easily supports a lot of developer tools. But the one among few disadvantages is 
+   that the performance is not up to the mark in low-end devices.
+
+2. Visual Studio
+   It is an IDE from Microsoft and is used to develop websites, web apps, and mobile apps. It has a wide functioning which allows it 
+   to develop almost anything. It also supports IntelliSense coding for C++. It can be used in both Windows and MAC and allows us to
+   create applications for any platform. It enables us to test apps and correct errors on the go. Visual Studio is available for free. 
+   Visual Studio comes with its own search engine which allows to search for the features of Visual Studio. The Team Foundation Server 
+   feature of Visual Studio is also very nice, it allows the user to have a checkpoint and roll back to the previous version. But the 
+   problem with Visual Studio is that it is a heavy software and takes a very long time to load.
+
+3. Eclipse
+   It is developed by the Eclipse Foundation. It functions by having a base workspace and an extensible plugin environment where we can
+   download and customize the workspace available. It supports debugging and profiling. It has a drag and drop functionality and allows 
+   us to analyze the code using static analysis. If you are comfortable in eclipse IDE, you can code in any famous language like Python,
+   Java, and C++. The OS supports for this IDE are Windows, Linux, and macOS. Eclipse is available for free to use.
+
+4. PyCharm
+   PyCharm is an IDE developed by JetBrains specifically for Python. It is best known for smart code completion, code inspections, 
+   on-the-fly error highlighting, and very quick fixes. It helps us to write neat and maintainable code. It also offers great support
+   for using frameworks like Django, Flask, Google App Engine, etc. It integrates with the IPython Notebook and we can also use 
+   scientific packages like Numpy and Matplotlib. PyCharm IDE is free to use. The OS supports for this IDE are Windows, macOS, and Linux.
+
+5. Atom
+   Atom is mainly used for web development. It is developed by GitHub. Atom is founded by GitHub’s founder Chris Wanstrath. It is a 
+   hundred percent free and open source. It is cross-platform and used for Windows, Linux, and macOS. It is highly customizable and
+   has many useful plugins. The main advantage with Atom is that it has an attractive interface, good color-coded syntax. But the 
+   disadvantage is that it suffers from high start-up time.
+
+6. Arduino IDE
+   Arduino is an IDE written in C and C++. It is mainly used to upload codes to Arduino boards. It supports almost all microcontroller 
+   development boards available in the market today. The code in Arduino is in the cloud so we can use it everywhere. Arduino IDE accepts
+   C and C++ languages because most of the device’s libraries are written in C++. But most of the underlying system of the Arduino is not
+   object-oriented so it uses both C and C++.
+
+7. NetBeans
+   NetBeans is developed by Apache Software Foundation and Oracle. It is an IDE for Java. It allows applications to be developed from 
+   components called modules. It is owned by the company which owns Java, so the support for Java developers would be excellent here.
+   Moreover, it is the official IDE for Java 8. It allows the users to build desktop, mobile, and web applications. The advantage of
+   NetBeans is that the autocomplete feature is very much useful and running the server application is very simple in NetBeans, but the
+   disadvantage is that it takes a lot of time to start.
+
+8. AWS Cloud9
+   It is developed by Amazon. It is an online IDE. AWS Cloud9 supports various programming languages like C, C++, Python, JavaScript,
+   etc. It is very easy to access AWS Cloud9 and collaborate. It allows us to run the development environment on a managed Amazon EC2
+   instance or other Linux servers available. In AWS Cloud9 we can also simulate the terminal with Linux commands, allowing parallel 
+   development for various developers. The disadvantage is that it is difficult to choose the instance for a project.
+
+9. Code::Blocks
+   It is developed by the Code::Blocks team. Code::Blocks is written in C++. It is open-source software. Code::Blocks support many 
+   compilers like GCC and Visual C++. It is very extensible and configurable. It is one of the best IDE for C and C++. The GNU GCC 
+   compiler of Code::Blocks is very fast and highly scalable. Code::Blocks is very easy to use for beginners. Code::Blocks offer more
+   flexibility and it is great as an open-source IDE. The disadvantage with Code::Blocks is that the design seems a bit dull.
+
+10.Jupyter Notebook
+   Jupyter Notebook, not actually a full-fledged IDE, is an open-source web-based application. It is widely used in data science, 
+   scientific computing, and machine learning. It is widely used with Python but it supports other languages as well. It is also used
+   as a presentation or educational tool. It also supports markdowns, so that we can add HTML components, audio, video, etc. It is
+   founded by Fernando Perez and Brian Granger.
+
+2. Advantages of IDE
+   Advantages
+1. IDE’s can be used to create software applications, drivers and utilities.
+2. It allows developing software in any programming language without spending much time on language syntax.
+3. IDE has the ability to correct syntaxes, gives a warning about memory leaks, assist in writing quality of code, etc.
+4. It has increased efficiency, where you can code faster with less effort, and its features help organise resources, prevent mistakes, and provide shortcuts.
+5. It supports collaboration, in which a group of programmers can easily work together within an IDE.
+6. It provides program resources that are easy.
+7. When creating applications, IDE manages resources such as library files, header files, etc., at specified locations.
+8. This includes pre-installed libraries for a specific programming language.
+9. That makes development easier by using syntax highlight features.
+10.It makes the creation of database applications easily.
+11.They provide services to sort, search, retrieve and process data in the database.
+12.IDE’s can able to translate code from high-level languages to the object code of the targeted platform at the compile or build stage.
+13.It helps to keep track of the code, generates code and allows searching.
+
+
+3. Shortcuts in PyCharm (Explain min 10)
+   ctrl + space => complete code
+   shift + ctrl + / => comments/ uncomment codes with block comments
+   ctrl + / => comments/ uncomment current line or selected block with line comments
+   shift + ctrl + enter => complete current statement
+   alt + ctrl + T => surround selected code fragment with if, while, try-catch or other construct
+   ctrl + Y => Delete line
+   ctrl + Delete => Delete to word end
+   ctrl + Backspace => Delete to word start
+   ctrl + D => duplicate line or block
+   ctrl + => Expand folding region at caret
+   shift + ctrl + J => Join lines
+   ctrl + enter => split line
+   shift + enter => new line
+   
+   
